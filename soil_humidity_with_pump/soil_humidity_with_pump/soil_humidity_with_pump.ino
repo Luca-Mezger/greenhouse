1c8fd25145f846b06a3cc5baea305dfc3f7f59c6
Luca <lucamezger88@gmail.com>
2024-09-09 11:51:19 +0200

    added pump call

--- soil_humidity_with_pump/soil_humidity_with_pump/soil_humidity_with_pump.ino
+++ soil_humidity_with_pump/soil_humidity_with_pump/soil_humidity_with_pump.ino
@@ -6,6 +6,8 @@
 #define STABILIZATION_THRESHOLD 0.5  // trheshold for detecting stabilization
 #define DELAY_TIME 10
 
+#define SOIL_MOISTURE_THRESHOLD 35
+
 float previousAverageSoilHumidity = 0; 
 
 float clip(float n, float lower, float upper) {
@@ -40,19 +42,39 @@ void setup() {
   Serial.begin(9600);
 }
 
+
+// Function to call when soil moisture is below the threshold
+void onLowSoilMoisture() {
+  Serial.println("Soil moisture is below threshold! pumping...");
+  //pump stuff
+}
+
 void loop() {
   float averageSoilHumidity = get_average_soil_humidity();
   
   Serial.print("Average Soil Humidity: ");
   Serial.println(averageSoilHumidity);
 
-  // check if moisture is stabilizing
-  if (is_stabilizing(averageSoilHumidity, previousAverageSoilHumidity)) {
-    Serial.println("Moisture level stabilizing.");
-  } else {
-    Serial.println("Moisture level changing.");
+  // Check if moisture is below threshold
+  if (averageSoilHumidity < SOIL_MOISTURE_THRESHOLD) {
+    onLowSoilMoisture(); 
   }
 
-  //update previous average
+  // Check if moisture is stabilizing
+  if (is_stabilizing(averageSoilHumidity, previousAverageSoilHumidity)) {
+    Serial.println("Moisture level stabilizing.");
+    
+    // If stabilizing, check moisture level again
+    if (averageSoilHumidity < SOIL_MOISTURE_THRESHOLD) {
+      onLowSoilMoisture(); 
+    } else {
+      Serial.println("Soil moisture is above threshold, good");
+    }
+  } else {
+    Serial.println("Moisture level changing. still waiting whether to pump again");
+  }
+
+  // Update previous average
   previousAverageSoilHumidity = averageSoilHumidity;
 }
+
